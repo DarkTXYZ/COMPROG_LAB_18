ea51f3ac7c7c41af2d6095914c7e66fc356b11b0
DarkTXYZ <pawaretmontfort@gmail.com>
2021-02-23 15:57:46 +0700

    Update

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -5,8 +5,11 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect *A , Rect *B){
+	double x = min(A->x + A->w, B->x + B->w) - max(A->x , B->x);
+	double y = min(A->y, B->y) - max(A->y - A->h , B->y - B->h);
+	if(!((x > 0 && y > 0) || (x < 0 && y < 0)))
+		return 0;
+	else
+		return x*y;
 }
